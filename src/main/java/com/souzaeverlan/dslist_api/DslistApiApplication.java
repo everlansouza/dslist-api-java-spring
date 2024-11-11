58238f2dd40a38a46acaf3a99c16d3fdb4e4c317
everlansouza <everlansoouza@gmail.com>
2024-11-11 10:47:33 -0300

    Project created

---
+++ src/main/java/com/souzaeverlan/dslist_api/DslistApiApplication.java
@@ -0,0 +1,13 @@
+package com.souzaeverlan.dslist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souzaeverlan/dslist_api/DslistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzaeverlan.dslist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
